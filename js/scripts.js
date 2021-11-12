7571d7047491ac677f6045e3707e9100d8c34a9a
Christen Weston <paleeserecycle@gmail.com>
2021-11-11 20:33:59 -0800

    Added findIndexOfMaxNumber to find the three most common words

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,6 @@ function noInputtedWord(word, text) {
         if (element.toLowerCase().includes(word.toLowerCase())) {
           wordCount++;
         }
-     //   console.log(word + ":" + wordCount)
       });
       return wordCount;
     }
@@ -46,38 +45,65 @@ function noInputtedWord(word, text) {
       })
     }
 
-    function firstInstanceOfWord(word, text) {
-      const textArray = text.split(" ");
-      let position = -1;
-      textArray.forEach(function(element, index) {
-        console.log(index);
-        if ((word === element) && (position === -1)) {
-          position = index;
-        }
-        console.log("Element " + element);
-        console.log("word: " + word);
-      });
-      console.log("Position:" + position);
-      return position;
-    }
-
     function wordList(text) {
       const textArray = text.split(" ");
-      let position = 0;
+      textArray.sort();
       let uniqueList = [];
       textArray.forEach(function(element, index) {
-        uniqueList.forEach(function(element2, index){
-        if (element === uniqueList[position]) {
-          console.log("Duplicate word: " + element);
-          position++;
-        }
-        if (element !== uniqueList[position]) {
-          uniqueList[position] = element;
-          console.log("Unique word: " + element);
-          position++;
-        }
+          if (uniqueList.indexOf(element) !== -1) {
+          }
+          else if (uniqueList.indexOf(element) === -1) {
+            uniqueList.push(element);
+          }
       });
       return uniqueList;
+    }
+
+   function createListOfOccurrences(uniqueList, passage, stableList) {
+     let uniqueListOccurs = [];
+      uniqueList.forEach(function(element, index) {
+          uniqueListOccurs.push(numberOfOccurrencesInText(element, passage));
+      })
+      console.log(uniqueListOccurs);
+      console.log(stableList);
+      findIndexOfMaxNumber(uniqueListOccurs, stableList);
+     return uniqueList;
+    }
+
+    function findIndexOfMaxNumber(numberedList, wordList) {
+      length1 = wordList.length;
+      let max = length1 -1;
+      let max2 = length1 -1;
+      let max3 = length1 -1;
+      numberedList.forEach(function(element, index) {
+        console.log("Index: " + index);
+        console.log("Here are currently most common: " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]);
+        if (parseInt(element) > parseInt(numberedList[max])) {
+          console.log("Here are currently most common: " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]);
+          console.log("element: " + element + "index" + index);
+          max3 = max2;
+          max2 = max;
+          max = index;
+          console.log("Here are currently most common after1: " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]);
+        }
+        else if (element <=parseInt(numberedList[max]) && element >= parseInt(numberedList[max2]) && element >= parseInt(numberedList[max3])) {
+          console.log("Here are currently most common: " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]);
+          console.log("element: " + element + "index" + index);
+          max3 = max2;
+          max2 = index;
+          console.log("Here are currently most common after2: " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]);
+        }
+        else if (element <=parseInt(numberedList[max]) && element <= parseInt(numberedList[max2]) && element >=parseInt(numberedList[max3])) {
+          console.log("Here are currently most common: " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]);
+          console.log("element: " + element + "index" + index);
+          max3 = index;
+          console.log("Here are currently most common after3: " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]);
+        }
+        else if (element < max) {
+        }
+      })
+        console.log("Here are the most common words " + wordList[max] + "," + wordList[max2] + "," + wordList[max3]); 
+        return max, max2, max3;
     }
 
     function includesRarestLetter(word) {
@@ -111,8 +137,8 @@ $(document).ready(function() {
     const word = $("#word").val();
     const wordCount = wordCounter(passage);
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
- //   const unique = uniqueArray(passage);
     const wordList1 = wordList(passage);
+    const uniqueListOccurs = createListOfOccurrences(wordList1, passage, wordList1);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
     $("#bolded-passage").html(boldPassage(word, passage));
